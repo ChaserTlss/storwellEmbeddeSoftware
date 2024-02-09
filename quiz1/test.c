db2f6f5f2a23c6619c337226c02b0ee593ba07b6
daimiao.chen@MBP <daniel-daimiao-chen@outlook.com>
2024-02-09 00:01:40 -0500

    devel: add quiz 1 test 2, pass

--- quiz1/test.c
+++ quiz1/test.c
@@ -3,6 +3,20 @@
 
 #include "calcPaper.h"
 
+#define check_result(number, result, colden_count, colden_result) \
+	if (number != colden_count) { \
+		printf("Test case failed: number != colden_count\n"); \
+		return 1; \
+	} \
+	for (int i = 0; i < number; i++) { \
+		if (result[i] != colden_result[i]) { \
+			printf("%s failed:[%d] result[%d] != colden_result[%d]\n", __func__, i, result[i], colden_result[i]); \
+			return 1; \
+		} \
+	} \
+	free(result); \
+	return 0;
+
 int test_case_1() {
 	unsigned int N = 6;
 	unsigned int M = 5;
@@ -12,24 +26,27 @@ int test_case_1() {
 	int numbers = 0;
 	int *result = calcPaper(N, M, &numbers);
 
-	if (numbers != colden_count) {
-		printf("Test case 1 failed: numbers != colden_count\n");
-		return 1;
-	}
+	check_result(numbers, result, colden_count, colden_result);
+}
 
-	for (int i = 0; i < numbers; i++) {
-		if (result[i] != colden_result[i]) {
-			printf("Test case 1 failed: result[%d] != colden_result[%d]\n", i, i);
-			return 1;
-		}
-	}
+int test_case_2() {
+	unsigned int N = 1;
+	unsigned int M = 1;
+	const int colden_count = 1;
+	int colden_result[colden_count] = {1};
 
-	return 0;
+	int numbers = 0;
+	int *result = calcPaper(N, M, &numbers);
 
+	check_result(numbers, result, colden_count, colden_result);
 }
 
 int main() {
 	if (test_case_1() == 0) {
+		printf("All test cases passed\n");
+	}
+
+	if (test_case_2() == 0) {
 		printf("All test cases passed\n");
 	}
 	return 0;
